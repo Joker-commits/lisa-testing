07b14eb93650326e7310e00e780d0ab2066dc338
Joker-commits <mkr896@gmail.com>
2023-01-27 14:37:48 +0530

    Bringup Bootleggers for Lisa

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lisa.mk
+    $(LOCAL_DIR)/bootleg_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lisa-user \
-    arrow_lisa-userdebug \
-    arrow_lisa-eng
+    bootleg_lisa-user \
+    bootleg_lisa-userdebug \
+    bootleg_lisa-eng
--- bootleg_lisa.mk
+++ bootleg_lisa.mk
@@ -11,18 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Bootleg stuff.
+$(call inherit-product, vendor/bootleggers/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# ArrowOS additions
-DEVICE_MAINTAINER := ALKALiKong
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_lisa
+PRODUCT_NAME := bootleg_lisa
 PRODUCT_DEVICE := lisa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 PRODUCT_MANUFACTURER := Xiaomi
+WITH_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
